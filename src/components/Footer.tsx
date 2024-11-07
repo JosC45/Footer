79c58b46e8157f1924dfd6d641cc3793265de347
Josue Caman <20b3010123@untels.edu.pe>
2024-11-06 22:41:09 -0500

    Centrado :v

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,10 @@ import { FaFacebook, FaInstagram, FaMapMarkerAlt, FaTiktok } from "react-icons/f
 const Footer: React.FC = () => {
   return (
     <footer className="text-gray-300 p-8">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 text-center md:text-left">
         
         {/* Logo */}
-        <div className="flex flex-col items-center justify-center mb-4 md:mb-0">
+        <div className="flex flex-col items-center md:items-center lg:items-start justify-center mb-4 md:mb-0">
           <div className="w-40 h-40 bg-cover bg-center rounded-full overflow-hidden mb-4">
             <img
               src="img/villa_29.jpg"
@@ -21,8 +21,8 @@ const Footer: React.FC = () => {
         {/* Menú de navegación */}
         <div className="flex flex-col items-center md:items-start">
           <h3 className="text-lg font-semibold text-gray-100 mb-4">Menú</h3>
-          <nav className="space-y-2">
-            <ul className="space-y-2 text-center md:text-left">
+          <nav>
+            <ul className="space-y-2">
               <li><a href="#" className="hover:text-gray-100">Inicio</a></li>
               <li><a href="#" className="hover:text-gray-100">Nosotros</a></li>
               <li><a href="#" className="hover:text-gray-100">Los más pedidos</a></li>
@@ -36,29 +36,29 @@ const Footer: React.FC = () => {
         {/* Horarios de Atención */}
         <div className="flex flex-col items-center md:items-start">
           <h3 className="text-lg font-semibold text-gray-100 mb-4">Horarios de Atención:</h3>
-          <ul className="space-y-1 text-center md:text-left">
+          <ul className="space-y-1">
             <li>Lunes a Domingo</li>
             <li>7:00 PM - 11:00 PM</li>
           </ul>
         </div>
 
         {/* Cómo llegar */}
-        <div className="flex flex-col items-center md:items-start mr-6"> {/* Añadí mr-6 aquí */}
+        <div className="flex flex-col items-center md:items-start">
           <h3 className="text-lg font-semibold text-gray-100 mb-4">Cómo llegar:</h3>
-          <p className="text-center md:text-left">Sec 3- Gru 29- Man F- Lote 3</p>
-          <p className="text-center md:text-left">Villa El Salvador, Lima</p><br />
+          <p>Sec 3- Gru 29- Man F- Lote 3</p>
+          <p>Villa El Salvador, Lima</p><br />
           <h3 className="text-lg font-semibold text-gray-100 mb-4">Referencia:</h3>
-          <p className="text-center md:text-left">Av. Mariano Pastor Sevilla</p>
-          <p className="text-center md:text-left">Entre Av. José Olaya y Balandra y Av. José Carlos Mariátegui</p>
+          <p>Av. Mariano Pastor Sevilla</p>
+          <p>Entre Av. José Olaya y Balandra y Av. José Carlos Mariátegui</p>
         </div>
 
         {/* Contacto y redes sociales */}
-        <div className="flex flex-col items-center md:items-start ml-6"> {/* Añadí ml-6 aquí */}
+        <div className="flex flex-col items-center md:items-start">
           <h3 className="text-lg font-semibold text-gray-100 mb-4">Contáctanos:</h3>
-          <p className="text-center md:text-left">987 280 900</p>
+          <p>987 280 900</p>
           <br />
           {/* Iconos de redes sociales */}
-          <div className="flex space-x-6 mb-6 justify-center">
+          <div className="flex space-x-6 mb-6">
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="transition-colors duration-300 hover:text-[#000000]">
               <FaFacebook size={30} />
             </a>
